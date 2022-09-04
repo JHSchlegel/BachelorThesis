4ba876a4bcb84336cb8928623bc4e9192912d2a1
Jan Schlegel <janschlegel@bluewin.ch>
2022-09-04 02:00:03 +0200

    t gjr garch and skew t ngarch

--- Bachelor Thesis Code/Scripts/ForecastingPortfolioVaR.R
+++ Bachelor Thesis Code/Scripts/ForecastingPortfolioVaR.R
@@ -28,21 +28,22 @@ numcores <- detectCores()
 
 # Univariate Normal GARCH(1,1) --------------------------------------------
 
-
-cl <- makePSOCKcluster(numcores)
-uni_normal.spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1,1)), mean.model = list(armaOrder=c(0,0), include.mean=TRUE),
+uni_normal.meanModel <- list(armaOrder=c(0,0), include.mean=TRUE)
+uni_normal.varModel <- list(model = "sGARCH", garchOrder = c(1,1))
+uni_normal.spec <- ugarchspec(variance.model = uni_normal.varModel, mean.model = uni_normal.meanModel,
                               distribution.model = "norm")
-uni_normal_GARCH <- ugarchroll(uni_normal.spec, portfolio.plret.ts, n.start = 1000, refit.every = 1,
+cl <- makePSOCKcluster(numcores)
+uni_normal_GARCH.roll <- ugarchroll(uni_normal.spec, portfolio.plret.ts, n.start = 1000, refit.every = 1,
                        refit.window = "moving", solver = "hybrid",
                        calculate.VaR = TRUE, VaR.alpha = c(0.01, 0.05),
                        cluster = cl, keep.coef = FALSE)
 
 stopCluster(cl)
 
-uni_normal_GARCH.VaR <- data.frame(Date = portfolio.plret.df$Date[-c(1:1000)], alpha.1perc = uni_normal_GARCH@forecast$VaR[,1],
-                                   alpha.5perc = uni_normal_GARCH@forecast$VaR[,2])
-head(uni_normal_GARCH.VaR)
-write.csv(uni_normal_GARCH.VaR, "Data\\Uni_Normal_GARCH.VaR.csv", row.names = FALSE)
+uni_normal_GARCH_VaR <- data.frame(Date = portfolio.plret.df$Date[-c(1:1000)], alpha.1perc = uni_normal_GARCH.roll@forecast$VaR[,1],
+                                   alpha.5perc = uni_normal_GARCH.roll@forecast$VaR[,2])
+head(uni_normal_GARCH_VaR)
+write.csv(uni_normal_GARCH_VaR, "Data\\VaR\\Uni_Normal_GARCH_VaR.csv", row.names = FALSE)
 
 
 
@@ -50,22 +51,62 @@ write.csv(uni_normal_GARCH.VaR, "Data\\Uni_Normal_GARCH.VaR.csv", row.names = FA
 
 
 lambda <- 0.94
-cl <- makePSOCKcluster(numcores)
-ewma.spec <- ugarchspec(variance.model=list(model="iGARCH", garchOrder=c(1,1)), 
-                        mean.model=list(armaOrder=c(0,0), include.mean=TRUE),  
+ewma.meanModel <- list(armaOrder=c(0,0), include.mean=TRUE)
+ewma.varModel <- list(model="iGARCH", garchOrder=c(1,1))
+ewma.spec <- ugarchspec(variance.model= ewma.varModel, mean.model= ewma.meanModel,  
                         distribution.model="norm", fixed.pars=list(omega=0, alpha = 1-lambda))
-ewma_GARCH <- ugarchroll(uni_normal.spec, portfolio.plret.ts, n.start = 1000, refit.every = 1,
+cl <- makePSOCKcluster(numcores)
+ewma_GARCH.roll <- ugarchroll(uni_normal.spec, portfolio.plret.ts, n.start = 1000, refit.every = 1,
                                refit.window = "moving", solver = "hybrid",
                                calculate.VaR = TRUE, VaR.alpha = c(0.01, 0.05),
                                cluster = cl, keep.coef = FALSE)
 stopCluster(cl)
-ewma_GARCH.VaR <- data.frame(Date = portfolio.plret.df$Date[-c(1:1000)], alpha.1perc = ewma_GARCH@forecast$VaR[,1],
-                             alpha.5perc = ewma_GARCH@forecast$VaR[,2])
-head(ewma_GARCH.VaR)
-write.csv(ewma_GARCH.VaR, "Data\\EWMA_GARCH.VaR.csv", row.names = FALSE)
+ewma_GARCH_VaR <- data.frame(Date = portfolio.plret.df$Date[-c(1:1000)], alpha.1perc = ewma_GARCH.roll@forecast$VaR[,1],
+                             alpha.5perc = ewma_GARCH.roll@forecast$VaR[,2])
+head(ewma_GARCH_VaR)
+write.csv(ewma_GARCH_VaR, "Data\\VaR\\EWMA_GARCH_VaR.csv", row.names = FALSE)
 
 
 # Univariate t-GJR-GARCH(1,1) ---------------------------------------------
+
+uni_t_gjr.meanModel <- list(armaOrder = c(0,0), include.mean = TRUE)
+uni_t_gjr.varModel <- list(model = "gjrGARCH", garchOrder = c(1,1))
+uni_t_gjr.spec <- ugarchspec(variance.model = uni_t_gjr.varModel, mean.model = uni_t_gjr.meanModel, distribution.model = "std")
+cl <- makePSOCKcluster(numcores)
+uni_t_gjr_GARCH.roll <- ugarchroll(uni_normal.spec, portfolio.plret.ts, n.start = 1000, refit.every = 1,
+                              refit.window = "moving", solver = "hybrid",
+                              calculate.VaR = TRUE, VaR.alpha = c(0.01, 0.05),
+                              cluster = cl, keep.coef = FALSE)
+stopCluster(cl)
+uni_t_gjr_GARCH_VaR <- data.frame(Date = portfolio.plret.df$Date[-c(1:1000)], alpha.1perc = uni_t_gjr_GARCH.roll@forecast$VaR[,1],
+                             alpha.5perc = uni_t_gjr_GARCH.roll@forecast$VaR[,2])
+head(uni_t_gjr_GARCH_VaR)
+write.csv(uni_t_gjr_GARCH_VaR, "Data\\VaR\\Uni_t_GJR_GARCH.csv", row.names = FALSE)
+
+
+# Univariate skew-t-NGARCH(1,1) -------------------------------------------
+
+
+uni_skewt_NGARCH.meanModel <- list(armaOrder = c(0, 0), include.mean = TRUE)
+uni_skewt_NGARCH.varModel <- list(model = "fGARCH", submodel = "NGARCH", garchOrder = c(1,1))
+uni_skewt_NGARCH.spec <- ugarchspec(variance.model = uni_skewt_NGARCH.varModel, mean.model = uni_skewt_NGARCH.meanModel, 
+                                    distribution.model = "sstd")
+cl <- makePSOCKcluster(numcores)
+uni_skewt_NGARCH.roll <- ugarchroll(uni_normal.spec, portfolio.plret.ts, n.start = 1000, refit.every = 1,
+                              refit.window = "moving", solver = "hybrid",
+                              calculate.VaR = TRUE, VaR.alpha = c(0.01, 0.05),
+                              cluster = cl, keep.coef = FALSE)
+stopCluster(cl)
+uni_skewt_NGARCH_VaR <- data.frame(Date = portfolio.plret.df$Date[-c(1:1000)], alpha.1perc = uni_skewt_NGARCH.roll@forecast$VaR[,1],
+                                  alpha.5perc = uni_skewt_NGARCH.roll@forecast$VaR[,2])
+head(uni_skewt_NGARCH_VaR)
+write.csv(uni_skewt_NGARCH_VaR, "Data\\VaR\\Uni_Skewt_NGARCH.csv", row.names = FALSE)
+
+
+###################### Don't forget univariate mix normal garch
+######################function for dataframe and head()
+
+
 
 
 
