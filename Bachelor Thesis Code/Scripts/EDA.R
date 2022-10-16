5646a8246ade3e3f45e91d1eafc76cd95496c321
Jan Schlegel <janschlegel@bluewin.ch>
2022-10-16 17:45:34 +0200

    Continued EDA

--- Bachelor Thesis Code/Scripts/EDA.R
+++ Bachelor Thesis Code/Scripts/EDA.R
@@ -1,6 +1,6 @@
-#=================================#
+###################################
 #### Exploratory Data Analysis ####
-#=================================#
+###################################
 
 library(tidyverse)
 library(yfR)
@@ -14,65 +14,74 @@ library(broom)
 # TODO: check for multivariate normality
 
 
+#------------------------------------------------------------------#
+########### Load Factors and Create Factors Dataframe ##############
+#------------------------------------------------------------------#
 
-# Load Factors and Create Factors Dataframe -------------------------------
-
-
-FFFactors <- read.csv("./Data/FamaFrenchFactorsDaily.csv", header = TRUE)
+FFFactors <- read.csv("Data/FamaFrenchFactorsDaily.csv", header = TRUE)
 View(FFFactors)
-MomFactor <- read.csv("./Data/MomentumFactorDaily.csv", header = TRUE)
+MomFactor <- read.csv("Data/MomentumFactorDaily.csv", header = TRUE)
 View(MomFactor)
 
-# TODO: include this section or not?
-# Check whether the dates are the same for the period 2nd January 2001 to 30th December 2011
-Date.fff.all <- as.Date(FFFactors.df$X, format = "%Y%m%d") 
-Date.fff <- Date.fff.all[(Date.fff.all >= "2001-01-02") & (Date.fff.all <= "2011-12-30")]
-Date.mom.all <- as.Date(MomFactor.df$X, format = "%Y%m%d")
-Date.mom <- Date.mom.all[(Date.mom.all >= "2001-01-02") & (Date.mom.all <= "2011-12-30")]
-all.equal(Date.fff, Date.mom)
+
+## Check whether the dates are the same for the period 2nd January 2001 to 30th December 2011
+Date_fff_all <- as.Date(FFFactors$X, format = "%Y%m%d") 
+Date_fff <- Date_fff_all[(Date_fff_all >= "2001-01-02") & (Date_fff_all <= "2011-12-30")]
+Date_mom_all <- as.Date(MomFactor$X, format = "%Y%m%d")
+Date_mom <- Date_mom_all[(Date_mom_all >= "2001-01-02") & (Date_mom_all <= "2011-12-30")]
+all.equal(Date_fff, Date_mom)
 # All dates are the same for this period
 
 ## Construct factor return dataframe for the period in question
-FFFactors.df <- FFFactors %>% 
+FFFactors_df <- FFFactors %>% 
   mutate(Date = X) %>% 
   select(-X) %>% 
   relocate(Date, .before = "Mkt.RF") %>% 
   relocate(RF, .before = "Mkt.RF") %>% 
   mutate(Date = as.Date(Date, format = "%Y%m%d")) %>%
   filter((Date >= "2001-01-02") & (Date <= "2011-12-30"))
-View(FFFactors.df)
+View(FFFactors_df)
 
-MomFactor.df <- MomFactor %>% 
+MomFactor_df <- MomFactor %>% 
   mutate(Date = X) %>% 
   select(-X) %>% 
   relocate(Date, .before = "Mom") %>% 
   mutate(Date = as.Date(Date, format = "%Y%m%d")) %>% 
   filter((Date >= "2001-01-02") & (Date <= "2011-12-30"))
-View(MomFactor.df)
+View(MomFactor_df)
 
 ## one dataframe for all factors
-FFCFactors.df <- data.frame(FFFactors.df, Mom = MomFactor.df$Mom)
-View(FFCFactors.df)
+FFCFactors_df <- data.frame(FFFactors_df, Mom = MomFactor_df$Mom)
+View(FFCFactors_df)
 ## Save newly created dataframe as csv file to allow for easy importing
-write.csv(FFCFactors.df, "Data\\FFCFactors.csv", row.names = FALSE)
+write.csv(FFCFactors_df, "Data\\FFCFactors.csv", row.names = FALSE)
 
 
-# Load Stock Percentage Log Returns ---------------------------------------
+
+#----------------------------------------------------------#
+########### Load Stock Percentage Log Returns ##############
+#----------------------------------------------------------#
 
 
-stocks.plret.df <- read.csv("./Data/StockPlrets.csv", header = TRUE)
-View(stocks.plret.df)
+stocks_plret_df <- read.csv("Data/StockPlrets.csv", header = TRUE)
+View(stocks_plret_df)
+
+all.equal(FFCFactors_df$Date, stocks_plret_df$Date)
+# all dates are the same for this period
+
 
 ## The following (commented out) section shows how this csv file has been created
 
 
-# Create Stock Percentage Log Returns Dataframe with Yahoo Finance Data ---------------------------
+#---------------------------------------------------------------------------------------------------#
+########### Creation of Stock Percentage Log Returns Dataframe with Yahoo Finance Data ##############
+#---------------------------------------------------------------------------------------------------#
 
 
-# import log returns from yahoo;
-# UTC now is called RTX due to fusion in April of 2020
+# # import log returns from yahoo;
+# # UTC now is called RTX due to fusion in April of 2020
 # tickers <- c("KO", "XOM", "GE", "IBM", "CVX", "RTX", "PG", "CAT", "BA", "MRK")
-# stock_data <- yf_get(tickers, first_date = "2000-12-29", last_date = "2011-12-30", 
+# stock_data <- yf_get(tickers, first_date = "2000-12-29", last_date = "2011-12-31",
 #                      freq_data = "daily",  type_return = "log")
 # head(stock_data)
 # ## Only the first log return for each of the stocks is NA
@@ -81,83 +90,97 @@ View(stocks.plret.df)
 # 
 # ## Bind and assign the stock name, date and log return of adjusted prices to the stock name
 # for (ticker_nr in 1:10){
-#   assign(tickers[ticker_nr], stock_data %>% filter(ticker == tickers[ticker_nr]) %>% 
+#   assign(tickers[ticker_nr], stock_data %>% filter(ticker == tickers[ticker_nr]) %>%
 #            select(ticker, ref_date, ret_adjusted_prices) %>% na.omit())
 # }
 # ## View PG as an example
 # View(PG)
 # 
 # ## Create dataframe with the log returns of all stocks
-# stocks.lret.df <- data.frame(Date = KO$ref_date, KO = KO$ret_adjusted_prices, XOM = XOM$ret_adjusted_prices, 
-#                              GE = GE$ret_adjusted_prices, IBM = IBM$ret_adjusted_prices, CVX = CVX$ret_adjusted_prices, 
+# stocks_lret_df <- data.frame(Date = KO$ref_date, KO = KO$ret_adjusted_prices, XOM = XOM$ret_adjusted_prices,
+#                              GE = GE$ret_adjusted_prices, IBM = IBM$ret_adjusted_prices, CVX = CVX$ret_adjusted_prices,
 #                              UTC = RTX$ret_adjusted_prices, PG = PG$ret_adjusted_prices, CAT = CAT$ret_adjusted_prices,
 #                              BA = BA$ret_adjusted_prices, MRK = MRK$ret_adjusted_prices)
-# View(stocks.lret.df)
+# View(stocks_lret_df)
 # 
 # ## Multiply all columns except Date by 100 to get percentage log returns
-# stocks.plret.df <- data.frame(Date = KO$ref_date, apply(stocks.lret.df[,-1], 2, function(x)100*x))
-# View(stocks.plret.df)
+# stocks_plret_df <- data.frame(Date = KO$ref_date, apply(stocks_lret_df[,-1], 2, function(x)100*x))
+# View(stocks_plret_df)
 # 
-# ## Save the created dataframe as a csv file to allow for easy importing and guarantee reproducibility
-# write.csv(stocks.plret.df, "Data\\StockPlrets.csv", row.names = FALSE)
+# # ## Save the created dataframe as a csv file to allow for easy importing and guarantee reproducibility
+# write.csv(stocks_plret_df, "Data\\StockPlrets.csv", row.names = FALSE)
 
 
 # Calculate Portfolio Percentage Log Returns of Equally Weighted Portfolio ---------------
 
 
-portfolio.weights <- rep(1/10, 10)
-portfolio.plret.df <- data.frame(Date = stocks.plret.df$Date, 
-                                 Portfolio = apply(stocks.plret.df[,-1], 1, function(x)x%*%portfolio.weights))
-View(portfolio.plret.df)
-## Alternatively: portfolio.plret.df <- data.frame(Date = stocks.plret.df$Date, Portfolio = apply(stocks.plret.df[,-1], 1, mean))
+portfolio_weights <- rep(1/10, 10)
+portfolio_plret_df <- data.frame(Date = stocks_plret_df$Date, 
+                                 Portfolio = apply(stocks_plret_df[,-1], 1, function(x)x%*%portfolio_weights))
+View(portfolio_plret_df)
+## Alternatively: portfolio_plret_df <- data.frame(Date = stocks_plret_df$Date, Portfolio = apply(stocks_plret_df[,-1], 1, mean))
 
 ## Visualise portfolio percentage log-returns
-par(mfrow=c(1,2))
-plot(as.Date(portfolio.plret.df$Date), portfolio.plret.df$Portfolio, type = "h", 
-     main = "Daily Percentage Log-Returns of Portfolio", xlab = "Date", ylab = "Percentage Log-Returns")
+par(mfrow=c(1,2), oma = c(0.2, 0.2, 0.2, 0.2), mar = c(5.1, 4.1, 4.1, 2.1),
+    cex.main = 0.9)
+plot(as.Date(portfolio_plret_df$Date), portfolio_plret_df$Portfolio, type = "h", 
+     main = "", xlab = "Date",
+     ylab = "Percentage Log-Returns")
+title("Panel A: Daily Percentage Log-Returns", line = 0.5)
 abline(h = 0, col = "grey")
 # periods of higher and lower volatility clearly visible
-hist(portfolio.plret.df$Portfolio, breaks = 50, prob = TRUE, xlab = "Percentage Log-Returns", 
-     main = "Histogram of Daily Percentage Log-Returns", col  ="grey95")
-lines(density(portfolio.plret.df$Portfolio), col = "black", lty = 1)
+hist(portfolio_plret_df$Portfolio, breaks = 50,  xlab = "Percentage Log-Returns", 
+     main = "", col  ="grey95", xlim = c(-10, 12))
+title("Panel B: Histogram of Daily Percentage Log-Returns", line = 0.5)
 # non-normal with very extreme values on either side
 
+max_return_ind <- which.max(portfolio_plret_df$Portfolio)
+portfolio_plret_df[max_return_ind, 1] #2008-10-28
 
-# Calculate Summary Statistics --------------------------------------------
+min_return_ind <- which.min(portfolio_plret_df$Portfolio)
+portfolio_plret_df[min_return_ind, 1] # 2008-10-15
+                   
+
+#-----------------------------------------------------#
+########### Calculate Summary Statistics ##############
+#-----------------------------------------------------#
 
 
 #' Summary Statistics
 #' @param dataframe Dataframe for which we want to calculate the summary statistics
 #' @param multiple.rets Boolean whether there are multiple return columns or not
-summary.statistics <- function(dataframe, multiple.rets =  TRUE){
-  descr.stats <- psych::describe(dataframe[,-1])
+summary_statistics <- function(dataframe, multiple.rets =  TRUE){
+  descr_stats <- psych::describe(dataframe[,-1])
   ## Conduct Jarque-Bera-Test
   ifelse(multiple.rets == TRUE,
          JB.test <- apply(dataframe[,-1], 2, FUN = jarque.bera.test),
          JB.test <- jarque.bera.test(dataframe[,-1]))
   ## Extract Jarque-Bera test statistic
   ifelse(multiple.rets == TRUE, 
-         JB.test.stat <- matrix(lapply(JB.test, function(x)return(x[1]))),
-         JB.test.stat <- matrix(JB.test[1]))
-  tab <- data.frame(Mean = descr.stats[,3], Median = descr.stats[,5], SD = descr.stats[,4], MAD = descr.stats[,7],
-                    Min = descr.stats[,8], Max = descr.stats[,9], Skew = descr.stats[,11], Kurt = descr.stats[,12], JB = JB.test.stat)
+         JB_test_stat <- matrix(lapply(JB.test, function(x)return(x[1]))),
+         JB_test_stat <- matrix(JB.test[1]))
+  tab <- data.frame(Mean = descr_stats[,3], Median = descr_stats[,5], SD = descr_stats[,4], MAD = descr_stats[,7],
+                    Min = descr_stats[,8], Max = descr_stats[,9], Skew = descr_stats[,11], Kurt = descr_stats[,12], JB = JB_test_stat)
   tab <- data.frame(Stock = colnames(dataframe)[-1], tab)
   return(tab)
 }
-summary.statistics(stocks.plret.df)
-summary.statistics(FFCFactors.df)
-summary.statistics(portfolio.plret.df, multiple.rets = FALSE)
+summary_statistics(stocks_plret_df)
+summary_statistics(FFCFactors_df)
+summary_statistics(portfolio_plret_df, multiple.rets = FALSE)
 
 
 
+#------------------------------------------#
+########### ACF Plots Factors ##############
+#------------------------------------------#
 
-# ACF Plots Factors -------------------------------------------------------
-
-par(mfrow = c(2,2),  cex = 0.8, oma = c(0.2, 0.2, 0.2, 0.2))
-names <- c("Market", "Size", "Value", "Momentum")
+par(mfrow = c(2,2),  cex = 0.8, oma = c(0.2, 0.2, 0.2, 0.2),
+    mar = c(2, 4.1, 4.1, 2.1))
+names <- c("Panel A: Market Factor", "Panel B: Size Factor", 
+           "Panel C: Value Factor", "Panel D: Momentum Factor")
 for (i in 1:4){
-  factors_acf <- acf(FFCFactors.df[,i+2], plot = FALSE)
-  abs_factors_acf <- acf(abs(FFCFactors.df[,i+2]), plot = FALSE)
+  factors_acf <- acf(FFCFactors_df[,i+2], plot = FALSE)
+  abs_factors_acf <- acf(abs(FFCFactors_df[,i+2]), plot = FALSE)
   plot(factors_acf, type = "l", ci.col = "black", lty = 1, lwd = 1.3,
        xlab = "Lag", main = "", ylab = "ACF")
   title(names[i], line = 0.5)
@@ -168,27 +191,28 @@ for (i in 1:4){
 }
 
 
-acf(FFCFactors.df[,3:6], ci.col = "black") # little to no auto-& crosscorrelation between returns
-acf(abs(FFCFactors.df[,3:6]), ci.col = "black") # high auto-& crosscorrelation between absolute returns
+acf(FFCFactors_df[,3:6], ci.col = "black") # little to no auto-& crosscorrelation between returns
+acf(abs(FFCFactors_df[,3:6]), ci.col = "black") # high auto-& crosscorrelation between absolute returns
 
+#-----------------------------------------#
+########### ACF Plots Shares ##############
+#-----------------------------------------#
 
-
-# ACF Plots Shares --------------------------------------------------------
 
 par(mfrow = c(4,3))
 for (i in 1:10){
-  stocks_acf <- acf(stocks.plret.df[,i+1], plot = FALSE)
-  abs_stocks_acf <- acf(abs(stocks.plret.df[,i+1]), plot = FALSE)
+  stocks_acf <- acf(stocks_plret_df[,i+1], plot = FALSE)
+  abs_stocks_acf <- acf(abs(stocks_plret_df[,i+1]), plot = FALSE)
   plot(stocks_acf, type = "l", ci.col = "black", lty = 1, lwd = 1.3,
        xlab = "Lag", main = "", ylab = "ACF")
-  title(colnames(stocks.plret.df)[i+1], line = 0.5)
+  title(colnames(stocks_plret_df)[i+1], line = 0.5)
   lines(x = 0:(length(abs_stocks_acf$acf)-1), abs_stocks_acf$acf, lty =3,
         lwd = 1.3)
   legend("topright", legend = c("returns", "absolute returns"), lty = c(1,4),
          bty = "n", lwd = c(1.3, 1.3))
 }
 
-acf(stocks.plret.df[,-1], ci.col = "black") # little to no auto-& crosscorrelation between returns
-acf(abs(stocks.plret.df[,-1]), ci.col = "black") # high auto-& crosscorrelation between absolute returns
+acf(stocks_plret_df[,-1], ci.col = "black") # little to no auto-& crosscorrelation between returns
+acf(abs(stocks_plret_df[,-1]), ci.col = "black") # high auto-& crosscorrelation between absolute returns
 
 
