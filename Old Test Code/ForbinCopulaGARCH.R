9bc54bf1e09d6bc6103b42fb92c9360a29e73492
Jan Schlegel <janschlegel@bluewin.ch>
2022-10-17 22:39:02 +0200

    joined dataframes

--- Old Test Code/ForbinCopulaGARCH.R
+++ Old Test Code/ForbinCopulaGARCH.R
@@ -13,3 +13,30 @@
 #TODO: VaR_t^p=-percentile(rpf,100p)
 
 # TODO: DCC which distribution? how to backtransform? 
+
+
+library(rugarch)
+library(rmgarch)
+library(tidyverse)
+library(xts)
+library(parallel)
+
+
+
+# Import Data and Create xts Objects --------------------------------------
+
+
+FFCFactors_df <- read.csv("./Data/FFCFactors.csv", header = TRUE)
+stocks_plret_df <- read.csv("./Data/StockPlrets.csv", header = TRUE)
+portfolio_plret_df <- data.frame(Date = stocks_plret_df$Date, Portfolio = apply(stocks_plret_df[,-1], 1, mean))
+
+## Create Dataframe with all stocks and market factors
+joined_df <- stocks_plret_df %>% 
+  full_join(FFCFactors_df)
+
+## Convert to time series
+FFCFactors_ts <- xts(FFCFactors_df[,-1], order.by = as.Date(FFCFactors_df$Date))
+stocks_plret_ts <- xts(stocks_plret_df[,-1], order.by = as.Date(stocks_plret_df$Date))
+portfolio_plret_ts <- xts(portfolio_plret_df[,-1], order.by = as.Date(portfolio_plret_df$Date))
+
+## Coefficients of Carhart Model
