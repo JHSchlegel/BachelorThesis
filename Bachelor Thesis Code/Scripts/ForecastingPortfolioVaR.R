f3407f91564820f2f25a3af9abea7164a0c64402
Jan Schlegel <janschlegel@bluewin.ch>
2023-01-29 17:50:19 +0100

    Added MN(3,2) and MN(2,2)

--- Bachelor Thesis Code/Scripts/ForecastingPortfolioVaR.R
+++ Bachelor Thesis Code/Scripts/ForecastingPortfolioVaR.R
@@ -2,12 +2,12 @@
 ########################## Forecasting Portfolio VaR ##########################
 #=============================================================================#
 
-if (!require(parallel)) install.packages("parallel")
-if (!require(rugarch)) install.packages("rugarch")
-if (!require(rmgarch)) install.packages("rmgarch")
-if (!require(tidyverse)) install.packages("tidyverse")
-if (!require(xts)) install.packages("xts")
-if (!require(MSGARCH)) install.packages("MSGARCH")
+if (!require(parallel)) install.packages("parallel") # parallel computing
+if (!require(rugarch)) install.packages("rugarch") # univariate GARCH models
+if (!require(rmgarch)) install.packages("rmgarch") # dcc garch
+if (!require(tidyverse)) install.packages("tidyverse") # data manipulation
+if (!require(xts)) install.packages("xts") # for multivariate time series
+if (!require(MSGARCH)) install.packages("MSGARCH") # for MN GARCH
 
 
 #--------------------------------------------------------#
@@ -16,8 +16,8 @@ if (!require(MSGARCH)) install.packages("MSGARCH")
 
 FFCFactors_df <- read.csv("./Data/FFCFactors.csv", header = TRUE)
 stocks_plret_df <- read.csv("./Data/StockPlrets.csv", header = TRUE)
-portfolio_plret_df <- data.frame(Date = stocks_plret_df$Date,
-                                 Portfolio =rowMeans(stocks_plret_df[,-1]))
+portfolio_plret_df <- read.csv("./Data/PortfolioPlrets.csv", header = TRUE)
+
 
 
 ## Convert to time series
@@ -36,13 +36,16 @@ numcores <- detectCores()-1
 ########### Univariate Normal GARCH(1,1) ###########
 #--------------------------------------------------#
 
-uni_normal_meanModel <- list(armaOrder=c(0,0), include.mean=TRUE)
+# no ARFIMA, just use unconditional mean
+uni_normal_meanModel <- list(armaOrder=c(0,0), include.mean=TRUE) 
 uni_normal_varModel <- list(model = "sGARCH", garchOrder = c(1,1))
 uni_normal_spec <- ugarchspec(
   variance.model = uni_normal_varModel, mean.model = uni_normal_meanModel, 
   distribution.model = "norm"
   )
 cl <- makePSOCKcluster(numcores)
+
+## Rolling window fitting
 uni_normal_GARCH_roll <- ugarchroll(
   uni_normal_spec, portfolio_plret_ts, refit.every = 1, refit.window = "moving", 
   n.start = 1000, solver = "hybrid", calculate_var = TRUE, 
@@ -57,6 +60,7 @@ uni_normal_GARCH_VaR <- data.frame(
   alpha_5perc = uni_normal_GARCH_roll@forecast$VaR[,2]
   )
 
+head(uni_normal_GARCH_VaR)
 write.csv(uni_normal_GARCH_VaR, "Data\\VaR\\Uni_Normal_GARCH_VaR.csv", 
           row.names = FALSE)
 
@@ -68,7 +72,8 @@ write.csv(uni_normal_GARCH_VaR, "Data\\VaR\\Uni_Normal_GARCH_VaR.csv",
 #------------------------------------------------------#
 
 
-lambda <- 0.94
+lambda <- 0.94 # as in RiskMetrics technical document
+# no ARFIMA, just use unconditional mean
 ewma_meanModel <- list(armaOrder=c(0,0), include.mean=TRUE)
 ewma_varModel <- list(model="iGARCH", garchOrder=c(1,1))
 uni_ewma_spec <- ugarchspec(
@@ -97,6 +102,7 @@ write.csv(uni_ewma_VaR, "Data\\VaR\\Uni_EWMA_VaR.csv", row.names = FALSE)
 ########### Univariate t-GJR-GARCH(1,1) ###########
 #-------------------------------------------------#
 
+# no ARFIMA, just use unconditional mean
 uni_t_gjr_meanModel <- list(armaOrder = c(0,0), include.mean = TRUE)
 uni_t_gjr_varModel <- list(model = "gjrGARCH", garchOrder = c(1,1))
 uni_t_gjr_spec <- ugarchspec(
@@ -127,6 +133,7 @@ write.csv(uni_t_gjr_GARCH_VaR, "Data\\VaR\\Uni_t_GJR_GARCH.csv",
 ########### Univariate skewt-GJR-GARCH(1,1) ###########
 #-----------------------------------------------------#
 
+# no ARFIMA, just use unconditional mean
 uni_skewt_gjr_meanModel <- list(armaOrder = c(0,0), include.mean = TRUE)
 uni_skewt_gjr_varModel <- list(model = "gjrGARCH", garchOrder = c(1,1))
 uni_skewt_gjr_spec <- ugarchspec(
@@ -154,7 +161,7 @@ write.csv(uni_skewt_gjr_GARCH_VaR, "Data\\VaR\\Uni_skewt_GJR_GARCH.csv",
 ########### Univariate Skewt-NGARCH(1,1) ###########
 #--------------------------------------------------#
 
-
+# no ARFIMA, just use unconditional mean
 uni_skewt_NGARCH_meanModel <- list(armaOrder = c(0, 0), include.mean = TRUE)
 uni_skewt_NGARCH_varModel <- list(model = "fGARCH", submodel = "NGARCH", 
                                   garchOrder = c(1,1))
@@ -186,6 +193,7 @@ write.csv(uni_skewt_NGARCH_VaR, "Data\\VaR\\Uni_Skewt_NGARCH.csv",
 # MN(k, g) as in Mixed Normal Conditional Heteroskedasticity by Haas, Mittnik
 # and Paolella (2004). doi: https://doi.org/10.1093/JJFINEC/NBH009
 
+## create cutom optimizer function for msgarch::fitML
 # default method in MSGARCH is "BFGS" which sometimes caused errors
 f_custom_optim <- function(vPw, f_nll, spec, data, do.plm){
   out <- stats::optim(vPw, f_nll, spec = spec, data = data,
@@ -193,12 +201,17 @@ f_custom_optim <- function(vPw, f_nll, spec, data, do.plm){
   return(out)
 }
 
-#' Title
+#' Diagonal Mix-Normal GARCH
 #'
-#' @param k 
-#' @param g 
+#' Implements MN(k,g) GARCH models as specified in Haas, Mittnik and 
+#' Paolella (2004)
 #'
-#' @return dataframe of VaR 
+#' @param k how many component densities
+#' @param g how many of component densities follow sGARCH process. The remaining
+#' k-g components are restricted to be constant
+#'
+#' @return dataframe of VaR with date in first column, 1% VaR in second column
+#' and 5% VaR in third column
 mn_k_g_garch <- function(k, g){
   window_size <- 1000
   n_windows <- dim(portfolio_plret_df)[1]-window_size
@@ -217,14 +230,22 @@ mn_k_g_garch <- function(k, g){
   message("===================================================================")
   message("                                MN(",k,",",g,")                    ")
   message("===================================================================")
-  for (i in 1:n_windows){
+  for (i in 316:n_windows){
     # rolling window fitting
-    fit_ml <- FitML(
+    fit_ml <- tryCatch(
+      {MSGARCH::FitML(
       spec = mn_k_g_garch_spec, 
       data = portfolio_plret_ts[i:(1000+i-1)],
       ctr = list(OptimFUN = f_custom_optim)
-      )
-    mn_k_g_garch_VaR[i,] <- Risk(fit_ml, alpha = c(0.01, 0.05), nahead = 1)$VaR
+      )},
+      error = function(cond) {
+        MSGARCH::FitML(
+          spec = mn_k_g_garch_spec, 
+          data = portfolio_plret_ts[i:(1000+i-1)]
+        ) # use BFGS instead of "L-BFGS-B"
+      })
+    mn_k_g_garch_VaR[i,] <- MSGARCH::Risk(fit_ml, alpha = c(0.01, 0.05), 
+                                          nahead = 1)$VaR
     # mn_k_g_garch_ES[i,] <- Risk(fit_ml, alpha = c(0.01, 0.05), nahead = 1)$ES
     message("completed: ", i, " of ", n_windows)
   }
@@ -234,15 +255,22 @@ mn_k_g_garch <- function(k, g){
   invisible(mn_k_g_garch_VaR_df)
 }
 
+## MN(2,2)
 mn_2_2_garch <- mn_k_g_garch(2,2)
+write.csv(mn_2_2_garch, "Data\\VaR\\Uni_MN_2_2_GARCH.csv", row.names = FALSE)
+
+## MN(3,2)
 mn_3_2_garch <- mn_k_g_garch(3,2)
-mn_3_3_garch <- mn_k_g_garch(3,3)
+write.csv(mn_3_2_garch, "Data\\VaR\\Uni_MN_3_2_GARCH.csv", row.names = FALSE)
+
+## MN(3,3)
+mn_3_3_garch <- mn_k_g_garch(3,3) #316
+write.csv(mn_3_3_garch, "Data\\VaR\\Uni_MN_3_2_GARCH.csv", row.names = FALSE)
+
 rugarch::VaRTest(alpha = 0.01, portfolio_plret_df[-c(1:1000),2], 
                  mn_2_2_garch[,2])
 
-write.csv(mn_2_2_garch, "Data\\VaR\\Uni_MN_2_2_GARCH.csv", row.names = FALSE)
-write.csv(mn_3_2_garch, "Data\\VaR\\Uni_MN_3_2_GARCH.csv", row.names = FALSE)
-write.csv(mn_3_3_garch, "Data\\VaR\\Uni_MN_3_2_GARCH.csv", row.names = FALSE)
+
 
 
 #--------------------------------------------------------#
@@ -250,19 +278,25 @@ write.csv(mn_3_3_garch, "Data\\VaR\\Uni_MN_3_2_GARCH.csv", row.names = FALSE)
 #--------------------------------------------------------#
 
 portfolio.weights <- rep(1/10, 10) # weight vector for 1/N portfolio
-dcc_meanModel <- list(armaOrder = c(0,0), include.mean = TRUE)
-dcc_varModel <- list(model = "sGARCH", garchOrder = c(1,1))
-dcc_uspec <- ugarchspec(variance.model = dcc_varModel, 
-                        mean.model = dcc_meanModel, distribution.model = "norm")
+# no ARFIMA, just use unconditional mean
+dcc_meanModel <- list(armaOrder = c(0,0), include.mean = TRUE) 
+dcc_varModel <- list(model = "sGARCH", garchOrder = c(1,1)) # variance model
+dcc_uspec <- ugarchspec(
+  variance.model = dcc_varModel, 
+  mean.model = dcc_meanModel, 
+  distribution.model = "norm"
+  )
+# model all stocks with same mean model and variance model
 dcc_mspec <- multispec(replicate(10, dcc_uspec))
+# normal DCC GARCH(1,1)
 dcc_spec <- dccspec(dcc_mspec, VAR = FALSE, model = "DCC", dccOrder = c(1,1), 
                     distribution =  "mvnorm")
 
 cl <- makePSOCKcluster(numcores)
-multi_dcc_roll <- dccroll(spec = dcc_spec, data = stocks_plret_ts, n.ahead = 1, 
-                          n.start = 1000, refit.every = 1, 
-                          refit.window = "moving", window.size = 1000,  
-                          cluster = cl)
+multi_dcc_roll <- dccroll(
+  spec = dcc_spec, data = stocks_plret_ts[c(1:1005),], n.ahead = 1, n.start = 1000, 
+  refit.every = 1, refit.window = "moving", window.size = 1000,  cluster = cl
+  )
 stopCluster(cl)
 
 n <- length(stocks_plret_df[,1])
@@ -273,13 +307,15 @@ dcc_mu_stocks <- matrix(NA, nrow = n_windows, ncol = 10)
 # Correlation matrix
 dcc_cov <- rcov(multi_dcc_roll) 
 
-for (i in 1:n_windows) {
+## calculating portfolio variance and extracting means of stocks
+for (i in 1:5){#n_windows) {
   dcc_sigma_pf[i] <- portfolio.weights %*% dcc_cov[,,i] %*% portfolio.weights
   dcc_mu_stocks[i,] <- multi_dcc_roll@mforecast[[i]]@mforecast$mu
 }
+## Calculating portfolio mean
 dcc_mu_pf <- apply(dcc_mu_stocks, 1, function(x) 100*log(mean(exp(x/100)-1)+1))
 
-  
+## Calculating VaR and saving it in data frame  
 multi_dcc_VaR <- data.frame(Date = portfolio_plret_df$Date[-c(1:1000)])
 multi_dcc_VaR$alpha_1perc <- dcc_mu_pf + sapply(dcc_sigma_pf, function(x) 
   x*qnorm(0.01, 0, 1, lower.tail = TRUE))
